0a77b0c87c01e7ce510bc9797bc3faa2026e9562
pavel_sol <59879066+Pavel-Sol@users.noreply.github.com>
2022-03-01 13:15:58 +0400

    feature/basic_app

---
+++ src/components/Board/Board.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Board: React.FC = () => {
+  return (
+    <>
+      <Container>ERETETET</Container>
+    </>
+  );
+};
+
+const Container = styled.div`
+  background: green;
+`;
+
+export default Board;
